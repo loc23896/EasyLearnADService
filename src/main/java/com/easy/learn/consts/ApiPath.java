6efc08ff35dc3f5749fc803a911d95d44275acbe
ITminhnh2509 <135216505+ITminhnh2509@users.noreply.github.com>
2023-12-28 14:26:31 +0700

    update path
    update path by minh

--- src/main/java/com/easy/learn/consts/ApiPath.java
+++ src/main/java/com/easy/learn/consts/ApiPath.java
@@ -13,15 +13,53 @@ public interface ApiPath {
     String USER_CREATE = API + "/user/create";
     String USER_UPDATE = API + "/user/update";
     String USER_DELETE = API + "/user/delete";
-    //trainersalarypaid
-    String TRAINER_SALARY_GET_ALL = API + "/trainersalary/get-all";
-    String TRAINER_SALARY_GET_ONE = API + "/trainersalary/get-one";
-    String TRAINER_SALARY_CREATE = API + "/trainersalary/create";
-    String TRAINER_SALARY_UPDATE = API + "/trainersalary/update";
-    String TRAINER_SALARY_DELETE = API + "/trainersalary/delete";
+    //WebsiteVenue
+    String WEBSITE_VENUE_GET_ALL = API + "/website-venue/get-all";
+    String WEBSITE_VENUE_GET_ONE = API + "/website-venue/get-one";
+    String WEBSITE_VENUE_CREATE = API + "/website-venue/create";
+    String WEBSITE_VENUE_UPDATE = API + "/website-venue/update";
+    String WEBSITE_VENUE_DELETE = API + "/website-venue/delete";
+    //
 
-    // end
+    //WebsiteFinances
+    String WEBSITE_FINANCES_GET_ALL = API + "/website-finances/get-all";
+    String WEBSITE_FINANCES_GET_ONE = API + "/website-finances/get-one";
+    String WEBSITE_FINANCES_CREATE = API + "/website-finances/create";
+    String WEBSITE_FINANCES_UPDATE = API + "/website-finances/update";
+    String WEBSITE_FINANCES_DELETE = API + "/website-finances/delete";
+    //
 
+    //WebsiteFinancesSummary
+    String WEBSITE_FINANCES_SUMMARY_GET_ALL = API + "/website-finances-summary/get-all";
+    String WEBSITE_FINANCES_SUMMARY_GET_ONE = API + "/website-finances-summary/get-one";
+    String WEBSITE_FINANCES_SUMMARY_CREATE = API + "/website-finances-summary/create";
+    String WEBSITE_FINANCES_SUMMARY_UPDATE = API + "/website-finances-summary/update";
+    String WEBSITE_FINANCES_SUMMARY_DELETE = API + "/website-finances-summary/delete";
+    //
+
+    //FinalSalarySummary
+    String FINAL_SALARY_SUMMARY_GET_ALL = API + "/final-salary-summary/get-all";
+    String FINAL_SALARY_SUMMARY_GET_ONE = API + "/final-salary-summary/get-one";
+    String FINAL_SALARY_SUMMARY_CREATE = API + "/final-salary-summary/create";
+    String FINAL_SALARY_SUMMARY_UPDATE = API + "/final-salary-summary/update";
+    String FINAL_SALARY_SUMMARY_DELETE = API + "/final-salary-summary/delete";
+    //
+
+    //StudentFinances
+    String STUDENT_FINANCES_GET_ALL = API + "/student-finances/get-all";
+    String STUDENT_FINANCES_GET_ONE = API + "/student-finances/get-one";
+    String STUDENT_FINANCES_CREATE = API + "/student-finances/create";
+    String STUDENT_FINANCES_UPDATE = API + "/student-finances/update";
+    String STUDENT_FINANCES_DELETE = API + "/student-finances/delete";
+    //
+
+    //TrainerSalaryPaid
+    String TRAINER_SALARY_PAID_GET_ALL = API + "/trainer-salary-paid/get-all";
+    String TRAINER_SALARY_PAID_GET_ONE = API + "/trainer-salary-paid/get-one";
+    String TRAINER_SALARY_PAID_CREATE = API + "/trainer-salary-paid/create";
+    String TRAINER_SALARY_PAID_UPDATE = API + "/trainer-salary-paid/update";
+    String TRAINER_SALARY_PAID_DELETE = API + "/trainer-salary-paid/delete";
+    //
     //trainersalarypaidSUMMARY
     String TRAINER_SALARY_SUMMARY_GET_ALL = API + "/trainer-salary-summary/get-all";
     String TRAINER_SALARY_SUMMARY_GET_ONE = API + "/trainer-salary-summary/get-one";
